c3283d115510c931e9b60799a0c830c8550a367c
Lucas Tavares <tilucast@gmail.com>
2020-02-12 16:01:05 -0300

    Modificações finais da primeira versão do projeto.

---
+++ README.md
@@ -0,0 +1,26 @@
+<img  src="https://img.shields.io/github/last-commit/tilucast/alura-frame?style=plastic" align="center"></img>
+
+<img  src="https://img.shields.io/badge/ESLint-6.0.1-blue" align="center"></img>
+
+# Instalação
+
+É um projeto simples, feito com HTML/CSS/Javascript. Apenas baixe os arquivos e abra **index.html**;
+Entretanto, estou usando uma configuração do <a href="https://eslint.org/">ESLint</a> com o <a href="https://prettier.io/">Prettier</a>; configuração esta feita pelo desenvolvedor <a href="https://github.com/wesbos">wesbos</a>.
+Link do repositório explicando como usar essa configuração:
+<a href="https://github.com/wesbos/eslint-config-wesbos">No-Sweat™ Eslint and Prettier Setup - with or without VS Code</a>.
+
+# Sobre o projeto
+
+É o projeto do curso ES6+ da Alura.
+Este projeto usa o modelo **MVC**, com **OOP**, além de muitas features novas do ES6+.
+
+O guia deste projeto é : <a href="https://github.com/flaviohenriquealmeida">Flávio Henrique de Souza Almeida</a>.
+
+# Modificações
+
+Não são muitas, porém dentre elas:
+
+<li>Modifiquei a forma como chamamos o listener do formulário. No projeto original, o guia chama o listener pelo HTML, já aqui eu chamo pelo próprio Javascript. (Preferência minha!)</li>
+<li>Algumas modificações em estilização.</li>
+<li>Adicionei um método que remove a notificação de negociação adicionada com sucesso.</li>
+<li>Visto que o curso foi dividido em 3 partes, assim que eu fizer tais partes, vou commita-las neste repositório, junto com minhas modificações.</li>
--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -1,26 +1,22 @@
 class NegociacaoController {
   constructor() {
-    /* let $ = document.querySelector.bind(document);
-        this._inputData = $('#data');
+    this._$ = document.querySelector.bind(document);
+    /* this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor'); */
 
-    const form = (this._form = document.querySelector('.form'));
-    this._dataFocus = document.querySelector('#data');
+    const form = (this._form = this._$('.form'));
+    this._dataFocus = this._$('#data');
     this._data = form.data;
     this._quantidade = form.quantidade;
     this._valor = form.valor;
     this._listaNegociacoes = new ListaNegociacoes();
 
-    this._negociacoesView = new NegociacoesView(
-      document.querySelector('#negociacoesView')
-    );
+    this._negociacoesView = new NegociacoesView(this._$('#negociacoesView'));
     this._negociacoesView._update(this._listaNegociacoes);
 
     this._mensagem = new Mensagem();
-    this._mensagemView = new MensagemView(
-      document.querySelector('#mensagemView')
-    );
+    this._mensagemView = new MensagemView(this._$('#mensagemView'));
   }
 
   adiciona() {
@@ -32,8 +28,9 @@ class NegociacaoController {
       this._mensagem.text = 'Negociação adicionada com sucesso!';
 
       this._mensagemView._update(this._mensagem);
-
       this._negociacoesView._update(this._listaNegociacoes);
+
+      setTimeout(this._retirarMensagem, 2000);
 
       this._limparForm();
     });
@@ -50,6 +47,11 @@ class NegociacaoController {
   _limparForm() {
     this._form.reset();
     this._dataFocus.focus();
+  }
+
+  _retirarMensagem() {
+    const msg = document.querySelector('#mensagemView');
+    msg.innerHTML = '';
   }
 }
 const negociacaoController = new NegociacaoController();
